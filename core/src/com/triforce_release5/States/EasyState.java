400e5157f11be4e4d7e266f65134c144c95ecc29
mbeemer <mbeemer17@hotmail.com>
2016-01-20 10:03:14 -0500

    .

--- core/src/com/triforce_release5/States/EasyState.java
+++ core/src/com/triforce_release5/States/EasyState.java
@@ -4,16 +4,24 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.triforce_release5.HUD;
 import com.triforce_release5.TbMenu;
 import com.triforce_release5.TbText;
 import com.triforce_release5.TriForceRelease5;
+
+import java.util.Iterator;
 
 /**
  * Created by Matthew on 12/9/2015.
@@ -31,6 +39,14 @@ public class EasyState implements Screen, InputProcessor{
     TbMenu tbMenu;
     TextButton TbBack;
 
+    Texture txTopTube, txBottTube; //Tube Stuff.
+    Sprite sprTopTube, sprBottTube;
+    OrthographicCamera camera;
+    Array<Sprite> arsprTopTube, arsprBottTube;
+    long lMoveTime, lMoveTime2;
+    int nSpawnTime;
+    float fTopTubeY;
+
     public EasyState(TriForceRelease5 triForceRelease5){ //References the main class.
         this.triForceRelease5=triForceRelease5;
         triForceRelease5.hud = new HUD(triForceRelease5.sbBatch);
@@ -45,6 +61,20 @@ public class EasyState implements Screen, InputProcessor{
         TbBack.setPosition(0, 0);
         textureAtlas = new TextureAtlas(Gdx.files.internal("Bird.pack"));
         animation = new Animation(1f/6f, textureAtlas.getRegions());
+
+        txTopTube = new Texture(Gdx.files.internal("TopTube.png")); //Tube Stuff.
+        txBottTube = new Texture(Gdx.files.internal("BottomTube.png"));
+        nSpawnTime = 100;
+        sprTopTube = new Sprite(txTopTube);
+        sprBottTube = new Sprite(txBottTube);
+        //Creating sprite and camera
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 800);
+        arsprTopTube = new Array<Sprite>();
+        arsprBottTube = new Array<Sprite>();
+        spawnTopTube();
+        spawnBottTube();
+
         stage.addActor(TbBack);
         Gdx.input.setInputProcessor(stage);
 
@@ -56,6 +86,23 @@ public class EasyState implements Screen, InputProcessor{
 
     }
 
+    private void spawnTopTube() {
+        Sprite sprTtube = new Sprite(txTopTube);
+        sprTtube.setX(800);
+        sprTtube.setY(MathUtils.random(400 - 200) + 250);
+        arsprTopTube.add(sprTopTube);
+        fTopTubeY = sprTtube.getY() - 150;
+        lMoveTime = TimeUtils.nanoTime();
+    }
+
+    private void spawnBottTube() {
+        Sprite sprBtube = new Sprite(txBottTube);
+        sprBtube.setX(750);
+        sprBtube.setY(fTopTubeY - 300);
+        arsprBottTube.add(sprBtube);
+        lMoveTime2 = TimeUtils.nanoTime();
+    }
+
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 0, 1); //Yellow background.
@@ -64,17 +111,41 @@ public class EasyState implements Screen, InputProcessor{
         stage.draw();
         triForceRelease5.hud.stage.draw(); //Draw the hud.
         fElapsedTime += Gdx.graphics.getDeltaTime();
+        sbBatch.setProjectionMatrix(camera.combined);
         sbBatch.begin();
-        sbBatch.draw(animation.getKeyFrame(fElapsedTime,true),150,180);
+        for (Sprite sprTtube : arsprTopTube) {
+            sbBatch.draw(sprTtube, sprTtube.getX(), sprTtube.getY());
+        }
+        for (Sprite sprBtube : arsprBottTube) {
+            sbBatch.draw(sprBtube, sprBtube.getX(), sprBtube.getY());
+        }
+        sbBatch.end();
+        if (TimeUtils.nanoTime() - lMoveTime > 100000000 * nSpawnTime) spawnTopTube();
+        Iterator<Sprite> iter = arsprTopTube.iterator();
+        while (iter.hasNext()) {
+            Sprite sprTtube = iter.next();
+            sprTtube.setX(sprTtube.getX() - (200) * Gdx.graphics.getDeltaTime());
+
+        }
+        if (TimeUtils.nanoTime() - lMoveTime2 > 100000000 * nSpawnTime) spawnBottTube();
+        Iterator<Sprite> iters = arsprBottTube.iterator();
+        while (iters.hasNext()) {
+            Sprite sprBtube = iters.next();
+            sprBtube.setX(sprBtube.getX() - (200) * Gdx.graphics.getDeltaTime());
+        }
+
+        sbBatch.begin();
+        sbBatch.draw(animation.getKeyFrame(fElapsedTime, true), 150, 180);
         sbBatch.end();
         triForceRelease5.hud.update();
-        if(TbBack.isPressed()){
+        if (TbBack.isPressed()) {
             triForceRelease5.currentState = TriForceRelease5.GameState.PLAY;
             triForceRelease5.updateState();
             triForceRelease5.hud.reset();
 
         }
     }
+
 
     @Override
     public void resize(int width, int height) {
